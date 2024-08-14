4626c4aa8d0dac520fca1e3ac2ed537e040db290
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-08-14 19:26:54 +0200

    events in html

--- toppings-pizza/app.js
+++ toppings-pizza/app.js
@@ -39,7 +39,7 @@
 // console.log(enlace[0].getAttribute('href'));
 // console.log(enlace[0].setAttribute('href', 'https://www.freecodecamp.org'));
 
-//Class
+// Class
 
 // const primerTopping = document.querySelector('.topping');
 
@@ -63,4 +63,27 @@ toppingNuevo.classList.add('topping', 'brown-bg')
 toppingNuevo.innerText = 'Queso Extra';
 
 listaDeToppings.append(toppingNuevo);
+
+
+// Manejo de Eventos
+
+const toppings = document.getElementsByClassName('topping')
+
+
+const mostrarClick = (e) => {
+    console.log(e.target.innerText)
+}
+for (const topping of toppings) {
+    topping.addEventListener('click', mostrarClick);
+} 
+
+// Arrow function form
+// for (const topping of toppings) {
+//     topping.addEventListener('click', (e) => {
+//         console.log(e.target.innerText)
+//     });
+// } 
+
+
+aceitunas.addEventListener('click', mostrarClick);
 
